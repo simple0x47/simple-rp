3b4ad82ab11a5f4291f56f291fa98bff8634afbc
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-12 19:37:58 +0200

    + does this work?

--- ui/client/browser.lua
+++ ui/client/browser.lua
@@ -1,13 +1,9 @@
-function onStart()
-    local width, height = guiGetScreenSize()
-    local browser = createBrowser(width, height, true, false)
+local width, height = guiGetScreenSize()
+local browser = createBrowser(width, height, true, false)
 
-    addEventHandler("onClientBrowserCreated", browser, function()
-        loadBrowserURL(source, "http://mta/local/client/index.html")
-        outputChatBox("Loaded URL")
-    end)
+addEventHandler("onClientBrowserCreated", browser, function()
+    loadBrowserURL(source, "http://mta/local/client/index.html")
+    outputChatBox("Loaded URL")
+end)
 
-    outputChatBox("Browser created")
-end
-
-addEventHandler("onClientResourceStart", root, onStart)
+outputChatBox("Browser created")
