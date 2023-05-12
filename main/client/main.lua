2f1a7553a054709a2fca4c129028a82511cbe312
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-12 23:38:28 +0200

    + extract resource specific html

--- main/client/main.lua
+++ main/client/main.lua
@@ -1,12 +1,6 @@
-addEvent("onClientClickTheButton", false)
-
 addEventHandler("onClientResourceStart", resourceRoot,
     function()
         local browser = exports.ui:pushBrowser("http://mta/local/client/files/main/index.html")
-
-        addEventHandler("onClientClickTheButton", browser, function()
-            outputChatBox("You clicked the button!")
-        end)
 
         showCursor(true)
     end)
