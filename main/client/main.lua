5a97c9072576990946e3d41cc788ebc21e67174d
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-13 00:13:21 +0200

    + overlay test

--- main/client/main.lua
+++ main/client/main.lua
@@ -2,5 +2,5 @@ addEventHandler("onClientResourceStart", resourceRoot,
     function()
         local browser = exports.ui:getWebBrowser()
 
-        executeBrowserJavascript(browser, "document.getElementById('body').innerHTML='<button>ABCD</button>';")
+        executeBrowserJavascript(browser, "document.getElementById('body').innerHTML='<div class=\"layer\" style=\"z-index: 1\"><h1 class=\"white\">1</h1></div><div class=\"layer\" style=\"z-index: 2\"><h1 class=\"grey\">2</h1></div>';")
     end)
