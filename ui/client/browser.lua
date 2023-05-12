7b40d8d5e9d200f6f8ef7e5344ffa9b8b4915c76
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-12 19:40:55 +0200

    f incorrect file url

--- ui/client/browser.lua
+++ ui/client/browser.lua
@@ -1,11 +1,6 @@
 local width, height = guiGetScreenSize()
 local browser = createBrowser(width, height, true, false)
 
-addEventHandler("onClientBrowserCreated", browser, function()
-    loadBrowserURL(source, "http://mta/local/client/index.html")
-    outputChatBox("Loaded URL")
-end)
-
 function webBrowserRender()
     --Render the browser on the full size of the screen.
     dxDrawImage(0, 0, width, height, browser, 0, 0, 0, tocolor(255, 255, 255, 255), true)
@@ -16,7 +11,7 @@ end
 addEventHandler("onClientBrowserCreated", browser,
     function()
         --After the browser has been initialized, we can load our file.
-        loadBrowserURL(browser, "http://mta/local/html/site.html")
+        loadBrowserURL(browser, "http://mta/local/client/index.html")
         --Now we can start to render the browser.
         addEventHandler("onClientRender", root, webBrowserRender)
     end
