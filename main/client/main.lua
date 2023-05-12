9696520068ae334bf412274a37629f23ab915e3d
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-12 23:54:11 +0200

    f missing semicolon

--- main/client/main.lua
+++ main/client/main.lua
@@ -2,5 +2,5 @@ addEventHandler("onClientResourceStart", resourceRoot,
     function()
         local browser = exports.ui:getWebBrowser()
 
-        executeBrowserJavascript(browser, "document.getElementById('body').innerHtml='<h1>Hello</h1><button>Whatsup</button>'")
+        executeBrowserJavascript(browser, "document.getElementById('body').innerHtml='<h1>Hello</h1><button>Whatsup</button>';")
     end)
