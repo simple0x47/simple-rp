da387bdc0b6a4c708992eaea94dcca3b078ad2d3
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-12 20:26:16 +0200

    + browser stack logic

--- ui/client/browser.lua
+++ ui/client/browser.lua
@@ -1,13 +1,50 @@
-local width, height = guiGetScreenSize()
-local browser = createBrowser(width, height, true, true)
+local screenWidth, screenHeight = guiGetScreenSize()
+local browserStack = {}
 
-function webBrowserRender()
-    dxDrawImage(0, 0, width, height, browser, 0, 0, 0, tocolor(255, 255, 255, 255), true)
+function pushBrowser()
+    local browser = createBrowser(screenWidth, screenHeight, true, true)
+
+    if #browserStack == 1 then
+        addEventHandler("onClientRender", root, renderWebBrowsers)
+    end
+
+    browserStack[#browserStack + 1] = browser
+    return browser
 end
 
-addEventHandler("onClientBrowserCreated", browser,
-    function()
-        loadBrowserURL(browser, "http://mta/local/client/index.html")
-        addEventHandler("onClientRender", root, webBrowserRender)
+function popBrowser()
+    if #browserStack == 0 then
+        return nil
     end
-)
+
+    local browser = browserStack[#browserStack]
+    browserStack[#browserStack] = nil
+
+    if #browserStack == 0 then
+        removeEventHandler("onClientRender", root, renderWebBrowsers)
+    end
+
+    return browser
+end
+
+function removeBrowser(browser)
+    if browser == nil then
+        return nil
+    end
+
+    for i = 1, #browserStack do
+        if browserStack[i] == browser then
+            browserStack[i] = browserStack[#browserStack]
+            return popBrowser()
+        end
+    end
+
+    return nil
+end
+
+function renderWebBrowsers()
+    for i = 1, #browserStack do
+        local browser = browserStack[i]
+        dxDrawImage(0, 0, screenWidth, screenHeight, browser, 0, 0, 0, tocolor(255, 255, 255, 255), true)
+    end
+end
