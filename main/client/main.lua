fd5e29b6b2bd3eea98a639a3737522a1cc56a89e
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-12 22:53:56 +0200

    + test intercommunication

--- main/client/main.lua
+++ main/client/main.lua
@@ -1,3 +1,9 @@
+addEvent("onClientClickTheButton", false)
+addEventHandler("onClientClickTheButton", root,
+    function()
+        outputChatBox("You clicked the button!")
+    end)
+
 addEventHandler("onClientResourceStart", resourceRoot,
     function()
         local browser = exports.ui:pushBrowser("http://mta/local/client/files/main/index.html")
