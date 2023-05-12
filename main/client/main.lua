05f63f41f05a310e3f6ab7d604a6ec754596ddea
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-12 23:40:11 +0200

    + specify file

--- main/client/main.lua
+++ main/client/main.lua
@@ -1,6 +1,6 @@
 addEventHandler("onClientResourceStart", resourceRoot,
     function()
-        local browser = exports.ui:pushBrowser("http://mta/local/client/files/main/index.html")
+        local browser = exports.ui:pushBrowser("http://mta/main/client/files/main/index.html")
 
         showCursor(true)
     end)
