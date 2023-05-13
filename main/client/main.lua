65b7f08c88b05d6477f29a5a96810268423b4064
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-13 10:21:22 +0200

    + try injecting html into browser

--- main/client/main.lua
+++ main/client/main.lua
@@ -1,6 +1,33 @@
-addEventHandler("onClientResourceStart", resourceRoot,
-    function()
-        local browser = exports.ui:getWebBrowser()
+function readHtml()
+    local file = fileOpen("files/index.html")
+    if not file then
+        outputDebugString("[MAIN] could not open index.html file", 2)
+    end
 
-        executeBrowserJavascript(browser, "document.getElementById('body').innerHTML='<div class=\"layer\" style=\"z-index: 1\"><h1 class=\"white\">1</h1></div><div class=\"layer\" style=\"z-index: 2\"><h1 class=\"grey\">2</h1></div>';")
-    end)
+    local size = fileGetSize(file)
+    local data = fileRead(file, size)
+    fileClose(file)
+
+    return data
+end 
+
+function onClientResourceStart()
+    local browser = exports.ui:getWebBrowser()
+
+    if not isWebBrowserDocumentReady then
+        addEventHandler("onClientBrowserDocumentReady", browser, function()
+            injectHtmlIntoBrowser(source)
+        end)
+        
+        return
+    end
+
+    injectHtmlIntoBrowser(browser)
+end
+addEventHandler("onClientResourceStart", resourceRoot, onClientResourceStart)
+
+function injectHtmlIntoBrowser(browser)
+    local html = readHtml()
+
+    executeBrowserJavascript(browser, "document.getElementById('body').innerHTML='" .. html .. "';")
+end
