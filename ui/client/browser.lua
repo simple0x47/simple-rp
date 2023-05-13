ba63a7c810352dd8055ebb96cb80fe913b146eca
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-13 10:21:13 +0200

    + avoid anonymous functions

--- ui/client/browser.lua
+++ ui/client/browser.lua
@@ -1,21 +1,26 @@
 local screenWidth, screenHeight = guiGetScreenSize()
-local browser = createBrowser(screenWidth, screenHeight, true, true)
+local browser = nil
 local isDocumentReady = false
 
-addEventHandler("onClientBrowserCreated", browser, 
+function onClientResourceStart()
+    browser = createBrowser(screenWidth, screenHeight, true, true)
+end
+addEventHandler("onClientResourceStart", resourceRoot, onClientResourceStart)
+
+function onClientBrowserCreated()
+    loadBrowserURL(source, "http://mta/local/client/files/index.html")
+    outputDebugString("[UI] browser created")
+    focusBrowser(source)
+
+    addEventHandler("onClientBrowserDocumentReady", source,
         function()
-            loadBrowserURL(source, "http://mta/local/client/files/index.html")
-            outputDebugString("[UI] browser created")
-            focusBrowser(source)
-
-            addEventHandler("onClientBrowserDocumentReady", source,
-                function()
-                    outputDebugString("[UI] browser document ready")
-                    isDocumentReady = true
-                end)
-
-            addEventHandler("onClientRender", root, renderWebBrowser)
+            outputDebugString("[UI] browser document ready")
+            isDocumentReady = true
         end)
+
+    addEventHandler("onClientRender", root, renderWebBrowser)
+end
+addEventHandler("onClientBrowserCreated", browser, onClientBrowserCreated)
 
 function getWebBrowser()
     return browser
