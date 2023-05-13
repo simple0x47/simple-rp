0bde507af018963ef802a406f78e3b0238af44f4
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-13 11:45:01 +0200

    + show sign in on start

--- main/client/main.lua
+++ main/client/main.lua
@@ -1,16 +1,4 @@
 function onClientResourceStart()
-    local browser = exports.ui:getWebBrowser()
-
-    if not exports.ui:isWebBrowserDocumentReady() then
-        addEventHandler("onClientBrowserDocumentReady", browser, function()
-            outputDebugString("[MAIN] injecting html into browser on document ready")
-            injectHtmlIntoBrowser()
-        end)
-        
-        return
-    end
-
-    outputDebugString("[MAIN] injecting html into browser on resource start")
-    injectHtmlIntoBrowser()
+    showSignIn()
 end
 addEventHandler("onClientResourceStart", resourceRoot, onClientResourceStart)
