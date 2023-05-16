5f907d2c241626130b4576a1d40fde4453b9efb7
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-05-16 22:36:01 +0200

    f trigger client event

--- main/server/models/sign_in.lua
+++ main/server/models/sign_in.lua
@@ -6,6 +6,6 @@
 addEvent("main:onTrySignIn", true)
 
 function onTrySignIn(username, password, rememberMe)
-    triggerClientEvent(source, "main:onSignInResult", false, { error = "Not implemented" })
+    triggerClientEvent(source, "main:onSignInResult", source, false, { error = "Not implemented" })
 end
 addEventHandler("main:onTrySignIn", root, onTrySignIn)
